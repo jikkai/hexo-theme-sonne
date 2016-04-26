d8e66b1201d35e04fce1967791b896effbe91578
十界 <sonne@asaki.me>
2016-04-26 10:13:40 +0800

    highlight theme => arta

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -3,4 +3,15 @@
     for (var i = 0; i < code.length; i++) {
         hljs.highlightBlock(code[i]);
     }
+
+    var img = document.querySelectorAll('.content img');
+    for (var i = 0; i < img.length; i++) {
+        var alt = document.createElement('span');
+        alt.innerHTML = img[i].alt;
+        alt.className = 'alt';
+        alt.onclick = function () {
+            
+        };
+        img[i].parentNode.appendChild(alt);
+    }
 })();
