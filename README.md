d950415c3c4cb53271bcbce3ddb511f07379d671
十界 <sonne@asaki.me>
2016-04-26 15:52:30 +0800

    🆕 duoshuo & disqus

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# hexo-theme-sonne
+# sonne
+
+Simple hexo theme.
+
+## Install
+
+Execute the following command and modify `theme` in `_config.yml` to `sonne`.
+
+```
+git clone https://github.com/jikkai/hexo-theme-sonne.git themes/sonne
+```
+
+## Update
+
+```
+cd themes/sonne
+git pull
+```
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,24 +1,2 @@
 (function () {
-    var img = document.querySelectorAll('.content img');
-    if (img.length > 0) {
-        var div = document.createElement('div');
-        div.className = 'imgMask';
-        div.onclick = function () {
-            this.style.display = 'none';
-        };
-        document.body.appendChild(div);
-
-        for (var i = 0; i < img.length; i++) {
-            var alt = document.createElement('span');
-            alt.innerHTML = img[i].alt;
-            alt.className = 'alt';
-            img[i].parentNode.appendChild(alt);
-            img[i].onclick = function () {
-                div.innerHTML = '<div>' +
-                                    '<img src="' + this.src + '">' +
-                                '</div>';
-                div.style.display = 'flex';
-            };
-        }
-    }
 })();
